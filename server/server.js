2fd0768ff65b737b754e6601092321c616f0c895
Dinesh S <dineshindevelope@gmail.com>
2025-02-04 11:23:38 +0530

    deployment

--- server/server.js
+++ server/server.js
@@ -10,7 +10,10 @@ const app = express();
 
 app.use(
   cors({
-    origin: ["http://localhost:5173"],
+    origin: [
+      "http://localhost:5173",
+      "https://mern-app-jwt-authentication-learning.vercel.app",
+    ],
     credentials: true,
   })
 );
